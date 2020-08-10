0933a9b06ae5e0b87b21f005054778f6a9136a15
klee8 <kate.d.lee@gmail.com>
2020-08-10 13:11:20 +1200

    added new make_ch.pl scripts to pipe

--- CHROnicle/setup_CHROnicle.sh
+++ CHROnicle/setup_CHROnicle.sh
@@ -6,13 +6,11 @@ mkdir Epichloe Epichloe/00rawGenome Epichloe/01Genomes
 # Eama708 Epichloe amarillans NFe708
 perl ./scripts/def_from_gff.pl ../data/Epichloe_Transect_Genomes/E.amarillans_NFe708/annotation/Epichloe_amarillans.gff3 Eama708
 perl ./scripts/make_prt.pl ../data/Epichloe_Transect_Genomes/E.amarillans_NFe708/annotation/Epichloe_amarillans.proteins.fa Eama708.def Eama708
-for i in {1..7}; do echo -ne "$i\t" >> Eama708.ch; done; echo >> Eama708.ch
-for i in {1..7}; do echo -ne "0\t" >> Eama708.ch; done; echo >> Eama708.ch
-for i in {1..7}; do echo -ne "0\t" >> Eama708.ch; done; echo >> Eama708.ch
 # sort new def file by chromosome and position
-sort -k3,3 -k4,4 Eama708.rdef > Eama708.srdef
+sort -k3,3 -k4,4n Eama708.rdef > Eama708.srdef
 mv Eama708.srdef Eama708.def
 rm Eama708.rdef
+perl ./scripts/make_ch.pl Eama708.def Eama708
 mv Eama708* Epichloe/01Genomes/
 
 # Ebro1 
@@ -27,19 +25,18 @@ mv Eama708* Epichloe/01Genomes/
 #data/Epichloe_Transect_Genomes/E.clarkii_1605_22/annotation/Ec_AT_rich.gff
 
 # Eely728
+
 perl ./scripts/def_from_gff.pl ../data/Epichloe_Transect_Genomes/E.elymi_NFe728/annotation/Epichloe_elymi_728.gff3 Eely728
 perl ./scripts/make_prt.pl ../data/Epichloe_Transect_Genomes/E.elymi_NFe728/annotation/Epichloe_elymi_728.proteins.fa Eely728.def Eely728
 # has mitochondria - numbered as chr 8 here
-for i in {1..8}; do echo -ne "$i\t" >> Eely728.ch; done; echo >> Eely728.ch
-for i in {1..8}; do echo -ne "0\t" >> Eely728.ch; done; echo >> Eely728.ch
-for i in {1..8}; do echo -ne "0\t" >> Eely728.ch; done; echo >> Eely728.ch
 # change chr 'm' to chr 8 in files too
 sed -i 's/\tm\t/\t8\t/g' Eely728.def
 sed -i 's/\tm\t/\t8\t/g' Eely728.prt
 # sort new def file by chromosome and position
-sort -k3,3 -k4,4 Eely728.rdef > Eely728.srdef
+sort -k3,3 -k4,4n Eely728.rdef > Eely728.srdef
 mv Eely728.srdef Eely728.def
 rm Eely728.rdef
+perl ./scripts/make_ch.pl Eely728.def Eely728
 mv Eely728* Epichloe/01Genomes/
 #data/Epichloe_Transect_Genomes/E.elymi_NFe728/annotation/Eel728_AT_rich.gff
 
@@ -60,14 +57,11 @@ mv Eely728* Epichloe/01Genomes/
 # EfesFl1 (note the only protein file available matches the v2 gff3 file)
 perl ./scripts/def_from_gff.pl ../data/Epichloe_Transect_Genomes/E.festucae_Fl1/Old/EfeFl1_v2.gff3 EfesFl1
 perl ./scripts/make_prt.pl ../data/Epichloe_Transect_Genomes/E.festucae_Fl1/EfFl1_proteins.faa EfesFl1.def EfesFl1
-
-for i in {1..7}; do echo -ne "$i\t" >> EfesFl1.ch; done; echo >> EfesFl1.ch
-for i in {1..7}; do echo -ne "0\t" >> EfesFl1.ch; done; echo >> EfesFl1.ch
-for i in {1..7}; do echo -ne "0\t" >> EfesFl1.ch; done; echo >> EfesFl1.ch
 # sort new def file by chromosome and position
-sort -k3,3 -k4,4 EfesFl1.rdef > EfesFl1.srdef
+sort -k3,3 -k4,4n EfesFl1.rdef > EfesFl1.srdef
 mv EfesFl1.srdef EfesFl1.def
 rm EfesFl1.rdef
+perl ./scripts/make_ch.pl EfesFl1.def EfesFl1
 mv EfesFl1* Epichloe/01Genomes/ 
 
 #data/Epichloe_Transect_Genomes/E.festucae_Fl1/EfeFl1_AT_rich.gff  

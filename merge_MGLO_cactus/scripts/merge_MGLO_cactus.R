289f7e4fde27dbc8aa62b8697c48a695f06af988
klee8 <kate.d.lee@gmail.com>
2020-08-18 18:42:35 +1200

    input mglo data

--- merge_MGLO_cactus/scripts/merge_MGLO_cactus.R
+++ merge_MGLO_cactus/scripts/merge_MGLO_cactus.R
@@ -2,11 +2,9 @@
 # bridge the two datasets 
 # AIM: use gene order from MGLO to set order of progressive cactus blocks
 
-
-
 library(tidyverse)
-
-
+#install.packages("comprehenr")
+library(comprehenr)
 
 #' Gene order supplied by MGLO in fasta-style format and re-formated to one
 #' line prior to reading into R (3 columns: genome, chr, gene_list)
@@ -17,13 +15,14 @@ get_gene_order <- function(x) {
   chr = x[2]
   listname <- paste(genome, chr, sep = "_")
   gene_list = x[3]
+  gene_list = x
   gene_order_vec <- gene_list %>% 
     str_trim( side = c("both")) %>% 
     strsplit( ",") %>%
     as.vector()
-  names(gene_order_vec) <- listname
   return(gene_order_vec)
 }
+
 
 
 #####  Get gene locations
@@ -36,9 +35,33 @@ colnames(tip_gen) <- c("genome", "chr", "gene_order")
 
 # create a list of vectors of gene order named by genome and chr ("genome_chr").
 tip_gene_lists <- apply(tip_gen, 1, get_gene_order)
-str(tip_gene_lists)
+
+# check number of genes in each genome
+gene_list_lengths <- matrix(to_vec(for(x in tip_gene_lists) c(x[1], x[2], length(x[[3]]))), ncol = 3, byrow = TRUE)
+colnames(gene_list_lengths) <- c("genome", "chr", "num_genes")
+gene_list_lengths <- as_tibble(gene_list_lengths)
+gene_list_lengths$chr <- as.numeric(gene_list_lengths$chr)
+gene_list_lengths$num_genes <- as.numeric(gene_list_lengths$num_genes)
+genes_per_genome <- gene_list_lengths %>% group_by(genome) %>% summarise(total_genes = sum(num_genes))
+genes_per_genome
 
 # read in gff files
+Eam708 <- read.delim("data/Eam708_Epichloe_amarillans_NFE708_38224169_v2.gff", stringsAsFactors = FALSE, header = FALSE, comment.char = "#")
+Ebr1 <- read.delim("data/Epichloe_bromicola_Nfe1.gff3", stringsAsFactors = FALSE, header = FALSE, comment.char = "#")
+Ecl1605 <- read.delim("data/Epichloe_clarkii_Hl.gff3", stringsAsFactors = FALSE, header = FALSE, comment.char = "#")
+Eel728 <- read.delim("data/Epichloe_elymi_728.gff3", stringsAsFactors = FALSE, header = FALSE, comment.char = "#")
+Eel732 <- read.delim("data/Eel732_Epichloe_elymi_NFE732_33820330_v2.gff", stringsAsFactors = FALSE, header = FALSE, comment.char = "#")
+EfeFl1 <- read.delim("data/EfFl1_v3.1.gff", stringsAsFactors = FALSE, header = FALSE, comment.char = "#")
+
+# subset to gene-data only
+Eam708 <- Eam708[Eam708$V3 == "gene", ]
+Ebr1 <- Ebr1[Ebr1$V3 == "gene",]
+Ecl1605 <- Ecl1605[Ecl1605$V3 == "gene",]
+Eel728 <- Eel728[Eel728$V3 == "gene",]
+Eel732 <- Eel732[Eel732$V3 == "gene",]
+EfeFl1 <- EfeFl1[EfeFl1$V3  == "gene",]
+
+
 
 
 # get gene locations for each genome and merge with 'numeric' gene names from MGLO
@@ -57,9 +80,17 @@ colnames(maf) <- c("block", "s", "genome.chr", "pos", "length", "strand", "chrle
 maf$s <- NULL
 # remove blocks with less than 500bp
 submaf <- maf %>% group_by(block) %>% filter(!any(mean(length) < 500))
+head(submaf, 20)
 
-# add list of genes to each block
 
+
+# check that list of genes from gff matches number of genes from maf (tip genome file)
+
+gene_list_lengths <- function(){
+  
+}
+
+# add list of genes positions to each block
 
 # check number of blocks without genes
 

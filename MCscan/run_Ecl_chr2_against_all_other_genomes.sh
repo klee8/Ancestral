1f1aa923697ace973e980cbad1f4dc6ba9efe6a3
klee8 <kate.d.lee@gmail.com>
2020-11-04 14:23:58 +1300

    compare chrs mapping to Eclarkii chr2

--- MCscan/run_Ecl_chr2_against_all_other_genomes.sh
+++ MCscan/run_Ecl_chr2_against_all_other_genomes.sh
@@ -25,6 +25,12 @@ cd results_Ecl_chr2
 rm *anchors*
 rm *last*
 
+# make a new copy of E clarkii files with changed name to compare it against itself
+cp Ecl.bed Eclx.bed
+cp Ecl.cds Eclx.cds
+sed -i 's/mrna/mrnaX/g' Ecl.bed
+sed -i 's/mrna/mrnaX/g' Ecl.cds
+
 # calculate synteny blocks for comparison with Ecl chr2
 python3 -m jcvi.compara.catalog ortholog Ecl Eam --cscore=.99 --no_strip_name
 python3 -m jcvi.compara.catalog ortholog Ecl Ebr --cscore=.99 --no_strip_name
@@ -32,7 +38,7 @@ python3 -m jcvi.compara.catalog ortholog Ecl Efe437 --cscore=.99 --no_strip_name
 python3 -m jcvi.compara.catalog ortholog Ecl Efl1 --cscore=.99 --no_strip_name
 python3 -m jcvi.compara.catalog ortholog Ecl Eel728 --cscore=.99 --no_strip_name
 python3 -m jcvi.compara.catalog ortholog Ecl Eel732 --cscore=.99 --no_strip_name
-python3 -m jcvi.compara.catalog ortholog Ecl Ecl --cscore=.70 --no_strip_name  # removed filter step to run Ecl against itself
+python3 -m jcvi.compara.catalog ortholog Ecl Eclx --cscore=.99 --no_strip_name     # renamed Ecl mrna to mrnaX in Eclx files in order to run it against itself 
 python3 -m jcvi.compara.catalog ortholog Ecl Ety --cscore=.99 --no_strip_name
 python3 -m jcvi.compara.catalog ortholog Ecl Etp --cscore=.99 --no_strip_name
 
@@ -55,13 +61,13 @@ echo '''Ecl1605_2
 Eam708_1,Eam708_2,Eam708_3,Eam708_4,Eam708_5,Eam708_6,Eam708_7''' > seqids
 
 echo '''# y, xstart, xend, rotation, color, label, va,  bed
- .6,     .1,    .8,       0,      , Ecl, top, Ecl.bed
+ .6,     .1,    .58,       0,      , Ecl, top, Ecl.bed
  .4,     .1,    .8,       0,      , Eam, top, Eam.bed
 # edges
 e, 0, 1, Ecl.Eam.anchors.simple''' > layout
 
 python3 -m jcvi.graphics.karyotype seqids layout
-mv karyotype.pdf karyotype_filt99_Eclchr2.Eam.pdf
+mv karyotype.pdf karyotype_proportional_chrs_Eclchr2.Eam.pdf
 
 
 ## Ebr
@@ -70,25 +76,25 @@ Ebr1_1,Ebr1_2,Ebr1_3,Ebr1_4,Ebr1_5,Ebr1_6,Ebr1_7''' > seqids
 
 echo '''# y, xstart, xend, rotation, color, label, va,  bed
  .6,     .1,    .8,       0,      , Ecl, top, Ecl.bed
- .4,     .1,    .8,       0,      , Ebr, top, Ebr.bed
+ .4,     .1,    .78,       0,      , Ebr, top, Ebr.bed
 # edges
 e, 0, 1, Ecl.Ebr.anchors.simple''' > layout
 
 python3 -m jcvi.graphics.karyotype seqids layout
-mv karyotype.pdf karyotype_filt99_Eclchr2.Ebr.pdf
+mv karyotype.pdf karyotype_proportional_chrs_Eclchr2.Ebr.pdf
 
 ## Ecl
 echo '''Ecl1605_2
 Ecl1605_1,Ecl1605_2,Ecl1605_3,Ecl1605_4,Ecl1605_5,Ecl1605_6,Ecl1605_7''' > seqids
 
 echo '''# y, xstart, xend, rotation, color, label, va,  bed
- .6,     .1,    .8,       0,      , Ecl, top, Ecl.bed
- .4,     .1,    .8,       0,      , Ecl, top, Ecl.bed
+ .6,     .1,    .22,       0,      , Ecl, top, Ecl.bed
+ .4,     .1,    .8,       0,      , Eclx, top, Eclx.bed
 # edges
-e, 0, 1, Ecl.Ecl.anchors.simple''' > layout
+e, 0, 1, Ecl.Eclx.anchors.simple''' > layout
 
 python3 -m jcvi.graphics.karyotype seqids layout
-mv karyotype.pdf karyotype_filt99_Eclchr2.Ecl.pdf
+mv karyotype.pdf karyotype_proportional_chrs_Eclchr2.Eclx.pdf
 
 
 ## Efe437 -- Ecl chr 2 maps to Efe437 chr 1 and 6
@@ -96,13 +102,13 @@ echo '''Ecl1605_2
 EfeE437_1,EfeE437_2,EfeE437_3,EfeE437_4,EfeE437_5,EfeE437_6,EfeE437_7''' > seqids
 
 echo '''# y, xstart, xend, rotation, color, label, va,  bed
- .6,     .1,    .8,       0,      , Ecl, top, Ecl.bed
+ .6,     .1,    .59,       0,      , Ecl, top, Ecl.bed
  .4,     .1,    .8,       0,      , Efe437, top, Efe437.bed
 # edges
 e, 0, 1, Ecl.Efe437.anchors.simple''' > layout
 
 python3 -m jcvi.graphics.karyotype seqids layout
-mv karyotype.pdf karyotype_filt99_Eclchr2.Efe437.pdf
+mv karyotype.pdf karyotype_proportional_chrs_Eclchr2.Efe437.pdf
 
 
 ## Eel728
@@ -111,12 +117,12 @@ Eel728_1,Eel728_2,Eel728_3,Eel728_4,Eel728_5,Eel728_6,Eel728_7''' > seqids
 
 echo '''# y, xstart, xend, rotation, color, label, va,  bed
  .6,     .1,    .8,       0,      , Ecl, top, Ecl.bed
- .4,     .1,    .8,       0,      , Eel728, top, Eel728.bed
+ .4,     .1,    .63,       0,      , Eel728, top, Eel728.bed
 # edges
 e, 0, 1, Ecl.Eel728.anchors.simple''' > layout
 
 python3 -m jcvi.graphics.karyotype seqids layout
-mv karyotype.pdf karyotype_filt99_Eclchr2.Eel728.pdf
+mv karyotype.pdf karyotype_proportional_chrs_Eclchr2.Eel728.pdf
 
 ## Eel732
 echo '''Ecl1605_2
@@ -124,38 +130,38 @@ Eel732_1,Eel732_2,Eel732_3,Eel732_4,Eel732_5,Eel732_6,Eel732_7''' > seqids
 
 echo '''# y, xstart, xend, rotation, color, label, va,  bed
  .6,     .1,    .8,       0,      , Ecl, top, Ecl.bed
- .4,     .1,    .8,       0,      , Eel732, top, Eel732.bed
+ .4,     .1,    .68,       0,      , Eel732, top, Eel732.bed
 # edges
 e, 0, 1, Ecl.Eel732.anchors.simple''' > layout
 
 python3 -m jcvi.graphics.karyotype seqids layout
-mv karyotype.pdf karyotype_filt99_Eclchr2.Eel732.pdf
+mv karyotype.pdf karyotype_proportional_chrs_Eclchr2.Eel732.pdf
 
 ## Efl1
 echo '''Ecl1605_2
 EfeFl1_1,EfeFl1_2,EfeFl1_3,EfeFl1_4,EfeFl1_5,EfeFl1_6,EfeFl1_7''' > seqids
 
 echo '''# y, xstart, xend, rotation, color, label, va,  bed
- .6,     .1,    .8,       0,      , Ecl, top, Ecl.bed
+ .6,     .1,    .58,       0,      , Ecl, top, Ecl.bed
  .4,     .1,    .8,       0,      , Efl1, top, Efl1.bed
 # edges
 e, 0, 1, Ecl.Efl1.anchors.simple''' > layout
 
 python3 -m jcvi.graphics.karyotype seqids layout
-mv karyotype.pdf karyotype_filt99_Eclchr2.Efl1.pdf
+mv karyotype.pdf karyotype_proportional_chrs_Eclchr2.Efl1.pdf
 
 ## Etp
 echo '''Ecl1605_2
 Etp76_1,Etp76_2,Etp76_3,Etp76_4,Etp76_5,Etp76_6,Etp76_7''' > seqids
 
 echo '''# y, xstart, xend, rotation, color, label, va,  bed
- .6,     .1,    .8,       0,      , Ecl, top, Ecl.bed
+ .6,     .1,    .29,       0,      , Ecl, top, Ecl.bed
  .4,     .1,    .8,       0,      , Etp, top, Etp.bed
 # edges
 e, 0, 1, Ecl.Etp.anchors.simple''' > layout
 
 python3 -m jcvi.graphics.karyotype seqids layout
-mv karyotype.pdf karyotype_filt99_Eclchr2.Etp.pdf
+mv karyotype.pdf karyotype_proportional_chrs_Eclchr2.Etp.pdf
 
 
 ## Ety
@@ -164,11 +170,11 @@ Ety1756_1,Ety1756_2,Ety1756_3,Ety1756_4,Ety1756_5,Ety1756_6,Ety1756_7''' > seqid
 
 echo '''# y, xstart, xend, rotation, color, label, va,  bed
  .6,     .1,    .8,       0,      , Ecl, top, Ecl.bed
- .4,     .1,    .8,       0,      , Ety, top, Ety.bed
+ .4,     .1,    .58,       0,      , Ety, top, Ety.bed
 # edges
 e, 0, 1, Ecl.Ety.anchors.simple''' > layout
 
 python3 -m jcvi.graphics.karyotype seqids layout
-mv karyotype.pdf karyotype_filt99_Eclchr2.Ety.pdf
+mv karyotype.pdf karyotype_proportional_chrs_Eclchr2.Ety.pdf
 
 cd ..
